558d43005602e02ecd0b4a3383aa165ecb768f2b
James Westervelt <james.a.westervelt@gmail.com>
2018-12-12 21:07:52 -0800

    Added function to collect information from Google Sign-In

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,10 @@
 var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+}
 //Global Vars
 //========================
 
@@ -14,16 +14,23 @@ var map;
 //Functions
 //==================
 function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+}
 
-        //debug
-      console.log("test");
+//debug
+console.log("test");
 
-
+//Collect information from Google Sign-In
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
 
 //Main
 //====================
