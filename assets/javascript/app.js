2c1d7e80aabb13010c8bee21048c84fe71f90c7a
myrean <dhughes1@uci.edu>
2018-12-14 23:39:06 -0800

    geo

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,17 +36,7 @@ function initMap() {
     center: providerLoc,
     zoom: 8
   });
-  // var geocoder = new google.maps.Geocoder();
-  // var address = "new york";
-  
-  // geocoder.geocode( { 'address': address}, function(results, status) {
-  
-  //   if (status == google.maps.GeocoderStatus.OK) {
-  //     var latitude = results[0].geometry.location.lat();
-  //     var longitude = results[0].geometry.location.lng();
-  //     alert(latitude);
-  //   } 
-  // }); 
+ 
 
   infoWindow = new google.maps.InfoWindow;
   // Try HTML5 geolocation.
