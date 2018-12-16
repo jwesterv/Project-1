c66acd0a1845055239a683c0862965e1a2869b65
myrean <dhughes1@uci.edu>
2018-12-16 10:56:59 -0800

    geocode update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,24 @@
-
-
 //Global Vars
 //========================
 var database;
+var providerLat;
+var providerLong;
+var user = [{
+  userId: 0,
+  isProvider: false,
+  first: "", // google first name
+  middle: "", // google middle name
+  last: "", // google last name
+  email: "", // google email
+  region: "", // google region
+  pic: "assets/images/pic.png", // google profile picture
+  width: 100, // picture width
+  height: 100, // picture height
+  make: "",
+  hasCable: "",
+
+}];
+
 
 
 
@@ -11,153 +27,140 @@ var database;
 
 $(document).ready(function () {
   initDb();
-  initMap();
-  latLong();
+  providerMap(40.785091, -73.968285);
+  geocodeAddress();
+
   console.log(database.ref());
 
-});
 
 
 
-//Functions
-//==================
 
-function initDb() {
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyC6eAPWuYgz1OEsS8mvrkNfu4XYduyD5aE",
-    authDomain: "project1-8e6c3.firebaseapp.com",
-    databaseURL: "https://project1-8e6c3.firebaseio.com",
-    projectId: "project1-8e6c3",
-    storageBucket: "project1-8e6c3.appspot.com",
-    messagingSenderId: "853012909302"
-  };
-  firebase.initializeApp(config);
-  database = firebase.database();
-}
+  //Functions
+  //==================
 
-
-function initMap() 
-{
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
-    center: {lat: -34.397, lng: 150.644}
-  });
-  var geocoder = new google.maps.Geocoder();
-// function initMap() {
-
-//   providerLat = 40.785091;
-//   providerLong = -73.968285;
-//   var providerLoc = { lat: providerLat, lng: providerLong };
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: providerLoc,
-//     zoom: 16
-//   });
+  function initDb() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyC6eAPWuYgz1OEsS8mvrkNfu4XYduyD5aE",
+      authDomain: "project1-8e6c3.firebaseapp.com",
+      databaseURL: "https://project1-8e6c3.firebaseio.com",
+      projectId: "project1-8e6c3",
+      storageBucket: "project1-8e6c3.appspot.com",
+      messagingSenderId: "853012909302"
+    };
+    firebase.initializeApp(config);
+    database = firebase.database();
+  }
 
 
-  document.getElementById('submit').addEventListener('click', function() {
-    geocodeAddress(geocoder, map);
-  });
-}
-// {
+  function providerMap(providerLat, providerLong) {
 
-//   providerLat = 40.785091;
-//   providerLong = -73.968285;
-//   var providerLoc = { lat: providerLat, lng: providerLong };
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: providerLoc,
-//     zoom: 8
-//   });
- 
-
-//   infoWindow = new google.maps.InfoWindow;
-//   // Try HTML5 geolocation.
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(function (position) {
-//       var pos = {
-//         lat: position.coords.latitude,
-//         lng: position.coords.longitude
-//       };
-
-//       infoWindow.setPosition(pos);
-//       infoWindow.setContent('Location found.');
-//       infoWindow.open(map);
-//       map.setCenter(pos);
-//     }, function () {
-//       handleLocationError(true, infoWindow, map.getCenter());
-//     });
-//   } else {
-//     // Browser doesn't support Geolocation
-//     handleLocationError(false, infoWindow, map.getCenter());
-//   }
-// }
-
-// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-//   infoWindow.setPosition(pos);
-//   infoWindow.setContent(browserHasGeolocation ?
-//     'Error: The Geolocation service failed.' :
-//     'Error: Your browser doesn\'t support geolocation.');
-//   infoWindow.open(map);
+    var providerLoc = { lat: providerLat, lng: providerLong };
+    var providerMap = new google.maps.Map(document.getElementById('map'), {
+      zoom: 8,
+      center: providerLoc
+    });
+    var geocoder = new google.maps.Geocoder();
+    console.log(geocoder);
 
 
-// }
+
+    document.getElementById('submit').addEventListener('click', function () {
+      geocodeAddress(geocoder, providerMap);
+    });
 
 
-//Creating a marker from user input
 
-function geocodeAddress(geocoder, resultsMap) {
-  var address = $('#address-input').val.trim;
-  geocoder.geocode({'address-input': address}, function(results, status) {
-    if (status === 'OK') {
-      resultsMap.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
+  }
+
+  function geocodeAddress(geocoder, resultsMap) {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({ 'address': address }, function gg(results, status) {
+      if (status === 'OK') {
+        resultsMap.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+          map: resultsMap,
+          position: results[0].geometry.location
+        });
+        console.log(marker.position.lat());
+        console.log(marker.position.lng());
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+  function gl() {
+
+    // Note: This example requires that you consent to location sharing when
+      // prompted by your browser. If you see the error "The Geolocation service
+      // failed.", it means you probably did not give permission for the browser to
+      // locate you.
+      var map, infoWindow;
+  
+        map = new google.maps.Map(document.getElementById('map2'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
+        });
+        infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+
+
+
+    {
+
+
+
     }
-  });
-}
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+      infoWindow.open(map);
 
 
+    }
+  }
 
-// function latLong() {
-
-
-//   database.ref().set(
-//     {
-//       lat: map.center.lat(),
-//       lng: map.center.lng()
-
-function mapsDb() {
-
-
-
-
-}
-
-
-
+  //Set data to Firebase
+  //   database.ref().set(
+  //     {
+  //       lat: map.center.lat(),
+  //       lng: map.center.lng()
 
   
-// }
 
 
-function geocodeAddress(geocoder, resultsMap) {
-  var address = document.getElementById('address').value;
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === 'OK') {
-      resultsMap.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
 
-geocodeAddress();
-
+});
