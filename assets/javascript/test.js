19226705fa9003a264d8593c0598909801e5ab73
James Westervelt <james.a.westervelt@gmail.com>
2018-12-19 16:17:15 -0800

    changed into creating login through firebase auth rather than google and created user variables and functions for login and logout

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -12,6 +12,73 @@ function initDb() {
   database = firebase.database();
 }
 
-gapi.load('auth2', function(){
-  gapi.auth2.init();
+var auth = firebase.auth();
+
+auth.signInWithEmailAndPassword(email, pass);
+
+auth.createUserWithEmailAndPassword(email, pass);
+
+auth.onAuthStateChanged(firebaseUser => { });
+
+//Gathering elements from form data
+
+var txtEmail = $("#txtEmail")
+var txtPassword = $("#txtPassword")
+var btnLogin = $("#btnLogin")
+var btnSignUp = $("#btnSignUp")
+var btnLogout = $("#btnLogout")
+
+//Signup Event
+
+$("btnSignUp").click(function () {
+  //email and password retrieval
+  var email = txtEmail.value;
+  var pass = txtPassword.value;
+  var auth = firebase.auth();
+
+  //Sign Up
+  var promise = auth.signUpWithEmailAndPassword(email, pass);
+  promise.catch(e => console.log(e.message));
 });
+
+//Login event
+
+$("btnLogIn").click(function () {
+  //email and password retrieval
+  var email = txtEmail.value;
+  var pass = txtPassword.value;
+  var auth = firebase.auth();
+
+  //Sign In
+  var promise = auth.signInWithEmailAndPassword(email, pass);
+  promise.catch(e => console.log(e.message));
+});
+
+//Log out
+
+btnLogout.addEventListener("click", e => {
+  fire.auth().signOut();
+})
+
+//Listener
+firebase.auth().onAuthStateChanged(function (firebaseUser) {
+  if (firebaseUser) {
+    console.log(firebaseUser);
+    // User is signed in.
+    var displayName = firebaseUser.displayName;
+    var email = firebaseUser.email;
+    var emailVerified = firebaseUser.emailVerified;
+    var photoURL = firebaseUser.photoURL;
+    var isAnonymous = firebaseUser.isAnonymous;
+    var uid = firebaseUser.uid;
+    var providerData = firebaseUser.providerData;
+    btnLogout.classList.remove("hide");
+  }
+  else {
+    //User is signed out
+    console.log("not logged in")
+  }
+})
+
+
+
