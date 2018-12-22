e8fe022ccda82c7d085ca6d92954bf7e5b71f093
James Westervelt <james.a.westervelt@gmail.com>
2018-12-21 16:51:13 -0800

    fixed conflicts

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -197,9 +197,6 @@ $(document).ready(function () {
       var colemail = "<div class='col'>" + data.user.email + "</div> ";
       var colphone = "<div class='col'>" + data.user.phone + "</div> ";
 
-<<<<<<< HEAD
-
-=======
       newRow.append(colName, colemail, colphone);
       $("#user").append(newRow);
 
@@ -210,7 +207,6 @@ $(document).ready(function () {
     database.ref().push(
       {
         user: user,
->>>>>>> b135ae0edfd282b6145c91af171028095875e5ec
 
 
       })
